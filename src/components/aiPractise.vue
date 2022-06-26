1c4c55651aa7c2d1bc8753ef842a9631121f476b
Dyrrachion <santatsuikwan@protonmail.com>
2022-06-26 22:42:15 +0800

    Update AIPractise

--- src/components/aiPractise.vue
+++ src/components/aiPractise.vue
@@ -9,12 +9,19 @@
         </div>
         <div id="content" class="content" style="height: 70%">
           <p>用户走进了展览大厅.....</p>
-          <div v-for="(item,index) in info" :key="index">
-
-            <div class="info_l" v-if="item.type == 'rightinfo'">
-
+          <div style="padding: 20px;">
+            <audio ref="audio" :src="audioSrc"></audio>
+          </div>
+          <div v-for="(item, index) in info" :key="index">
+            <div class="info_l" v-if="item.type === 'rightinfo'">
               <div class="con_r con_text">
-                <span class="con_l">{{item.content}}</span>
+                <span class="con_l">
+                 <div >
+                  <el-button v-if="!item.state" type="primary" @click="handlePlay(index)" >播放录音</el-button>
+                 </div>
+                  {{item.content}}
+                </span>
+
                 <span class="circle circle_l">
                   <img src class="pic_l" />
                 </span>
@@ -22,6 +29,7 @@
               <div class="time_l">{{item.time}}</div>
 
             </div>
+
             <div class="info_r info_default" v-if="item.type == 'leftinfo'" style="width:100%;float:left " >
 
               <span class="circle circle_r"></span>
@@ -30,16 +38,10 @@
 
                 <div v-for="(item2,index) in item.question" :key="index" >
 
-                  <div class="con_que" @click="clickRobot(item2.content,item2.id)">
-
-                    <div class="czkj-question-msg">
-                      {{item2.index}}
-                      {{item2.content}}
-
-                    </div>
-                  </div>
                 </div>
+
               </div>
+
               <div class="time_r" style="float: left">{{item.time}}</div>
          </div>
 
@@ -48,26 +50,37 @@
 
         <div class="setproblem" >
 
+        <div>
+
           <textarea
             placeholder="请输入您的回答..."
-            style="height: 68px;width: 100%;resize:none;padding-right:80px;outline: none;border-color:#ccc;border-radius:5px;"
+            style="height: 68px; width: 85%;float:left;resize:none;padding-right:80px;outline: none;border-color:#ccc;border-radius:5px;"
             id="text"
             v-model="customerText"
             @keyup.enter="sentMsg()"
           >
-
           </textarea>
-          <button @click="sentMsg()" class="setproblems">
-
+          <button @click="sentMsg()" class="setproblems" style="float: left">
             <span style="vertical-align: 4px;">发 送</span>
           </button>
+          </div>
+        </div>
+
+        <div style="font-size:14px"  >
           <i class="el-icon-mic" style="font-size: 25px"></i>
+         <el-button type="primary" @click="handleStart">开始录音</el-button>
+         <el-button type="warning" @click="handleStop">停止录音</el-button>
+          <el-button type="error" @click="handleDestroy">删除录音</el-button>
+          录音时长：{{ recorder && recorder.duration.toFixed(4) }}
         </div>
       </div>
+
     </div>
   </div>
 </template>
 <script>
+import Recorder from 'js-audio-recorder'
+
 export default {
   name: "onlineCustomer",
   components: {},
@@ -76,73 +89,106 @@ export default {
     return {
       customerText: "",
       info: [
-        {
-
-        },
+        {},
       ],
       timer: null,
       robotQuestion: [
-        { id: 1, content: "测试回答", index: 1 },
+        {id: 1, content: "robotQuestion", index: 1},
       ],
       robotAnswer: [
         {
           id: 1,
           content:
-            "测试回答",
+            "robotAnswer",
           index: 1,
         },
-        { id: 2, content: "测试", index: 2 },
-        { id: 3, content: "测试", index: 3 },
-        {
-          id: 4,
-          content: "3333333",
-          index: 4,
-        },
-        { id: 5, content: "44444444", index: 5 },
       ],
+      recorder: null,
+      playTime: 0,
+      audioSrc: '',
+      recordList:[],
+
     };
   },
   created() {
     this.showTimer();
+    this.recorder = new Recorder({
+      sampleBits: 8, // 采样位数，支持 8 或 16，默认是16
+      sampleRate: 11025, // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
+      numChannels: 1 // 声道，支持 1 或 2， 默认是1
+      // compiling: false,(0.x版本中生效,1.x增加中)  // 是否边录边转换，默认是false
+    })
   },
   watch: {},
   methods: {
     // 用户发送消息
     sentMsg() {
-      clearTimeout(this.timer);
-      this.showTimer();
-      let text = this.customerText.trim();
-      if (text != "") {
+       clearTimeout(this.timer);
+       this.showTimer();
+       let text = this.customerText.trim();
+       if (text == "") {
+        if (this.recorder == null || this.recorder.duration === 0) {
+          this.$message({
+            message: '请先录音',
+            type: 'error'
+          })
+          return false
+        }
+        this.recorder.pause() // 暂停录音
+        this.timer = null
+        console.log('上传录音')// 上传录音
+
+         //上传录音
+        const formData = new FormData()
+        const blob = this.recorder.getWAVBlob()// 获取wav格式音频数据
+        // 此处获取到blob对象后需要设置fileName满足当前项目上传需求，其它项目可直接传把blob作为file塞入formData
+        const newbolb = new Blob([blob], {type: 'audio/wav'})
+        const fileOfBlob = new File([newbolb], new Date().getTime() + '.wav')
+        formData.append('file', fileOfBlob)
+        const url = window.URL.createObjectURL(fileOfBlob)
+        this.src = url;
+        //发送语音和文字消息
+        text=this.gettext()
+        // const axios = require('axios')
+        // axios.post(url, formData).then(res => {
+        //   console.log(res.data.data[0].url)
+        // })
+
+
         var obj = {
           type: "rightinfo",
           time: this.getTodayTime(),
+          state:false,
+          content:text,
+          src:url,
+
+        };
+        this.info.push(obj);
+        //顾客回答
+        this.appendRobotMsg(this.customerText);
+        this.customerText = "";
+      } else {
+        var obj = {
+          type: "rightinfo",
+          state:true,
+          time: this.getTodayTime(),
           content: text,
+
         };
         this.info.push(obj);
         this.appendRobotMsg(this.customerText);
         this.customerText = "";
-        this.$nextTick(() => {
-          var contentHeight = document.getElementById("content");
-          contentHeight.scrollTop = contentHeight.scrollHeight;
-        });
       }
+      ;
     },
     // 机器人回答消息
     appendRobotMsg(text) {
       clearTimeout(this.timer);
       this.showTimer();
       text = text.trim();
-      let answerText = "";
-      let flag;
-      for (let i = 0; i < this.robotAnswer.length; i++) {
-        if (this.robotAnswer[i].content.indexOf(text) != -1) {
-          flag = true;
-          answerText = this.robotAnswer[i].content;
-          break;
-        }
-      }
-      if (flag) {
-        let obj = {
+      //根据传来的消息从题库抽取问题
+      let answerText = "题库问题1";
+      let obj = {
           type: "leftinfo",
           time: this.getTodayTime(),
           name: "robot",
@@ -150,22 +196,11 @@ export default {
           question: [],
         };
         this.info.push(obj);
-      } else {
-        answerText = "您可能想问：";
-        let obj = {
-          type: "leftinfo",
-          time: this.getTodayTime(),
-          name: "robot",
-          content: answerText,
-          question: this.robotQuestion,
-        };
-        this.info.push(obj);
-      }
-      this.$nextTick(() => {
-        var contentHeight = document.getElementById("content");
-        contentHeight.scrollTop = contentHeight.scrollHeight;
-      });
-    },
+      },
+      // this.$nextTick(() => {
+      //   var contentHeight = document.getElementById("content");
+      //   contentHeight.scrollTop = contentHeight.scrollHeight;
+      // });
     sentMsgById(val, id) {
       clearTimeout(this.timer);
       this.showTimer();
@@ -194,10 +229,6 @@ export default {
         contentHeight.scrollTop = contentHeight.scrollHeight;
       });
     },
-    // 点击机器人的单个问题
-    clickRobot(val, id) {
-      this.sentMsgById(val, id);
-    },
     // 结束语
     endMsg() {
       let happyEnding = {
@@ -213,8 +244,9 @@ export default {
       });
 
     },
+
     showTimer() {
-      this.timer = setTimeout(this.endMsg, 1000*60);
+      this.timer = setTimeout(this.endMsg, 1000 * 60);
     },
     getTodayTime() {
       // 获取当前时间
@@ -245,11 +277,70 @@ export default {
         seconds;
       return time;
     },
-  },
-  mounted() {},
-  props: {},
-  destroyed() {},
-};
+    gettext(){
+      const arr = ['happy','sad','normal']
+      this.mood = arr[Math.floor(Math.random()*3)]
+        let text=this.mood;
+        return text;
+    },
+
+    //开始录音
+    handleStart() {
+      this.recorder = new Recorder()
+      Recorder.getPermission().then(() => {
+        console.log('开始录音')
+        this.recorder.start() // 开始录音
+      }, (error) => {
+        this.$message({
+          message: '请先允许该网页使用麦克风',
+          type: 'info'
+        })
+        console.log(`${error.name} : ${error.message}`)
+      })
+    },
+    //停止录音
+    handleStop() {
+      console.log('停止录音')
+      this.recorder.stop();
+      const blob = this.recorder.getWAVBlob();
+      const newBlob = new Blob([blob], {type: 'audio/wav'});
+      const url = URL.createObjectURL(newBlob);
+      console.log(url);
+      this.recordList.push(url);
+    },
+    //删除录音
+    handleDestroy() {
+      console.log('删除')
+      this.recorder.destroy() // 毁实例
+      this.recordList.pop();
+      this.timer = null
+    },
+    //播放录音
+    handlePlay(index) {
+      let selfIndex = Math.floor(index / 2);
+      console.log(selfIndex);
+      this.$refs.audio.src = this.recordList[selfIndex];
+      this.$refs.audio.play();
+      // this.recorder.play();
+      // 播放时长
+      this.timer = setInterval(() => {
+        try {
+          this.playTime = this.recorder.getPlayTime()
+        } catch (error) {
+          this.timer = null
+        }
+      }, 100)
+
+
+
+    },
+    mounted() {
+    },
+    destroyed() {
+
+    },
+  }
+}
 </script>
 
 <style lang="scss">
